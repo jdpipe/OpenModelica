31ff8b865e12eb00ce62af767c4a4d7cc3f7fe5e
John Pye <john.pye@anu.edu.au>
2022-02-02 12:45:23 +1100

    removing szip dependency on windows MSYS, not needed for current hdf5.

--- OMCompiler/omc_config.h
+++ OMCompiler/omc_config.h
@@ -39,115 +39,117 @@
 
 #if !defined(MSYS2_AUTOCONF) && (defined(__MINGW32__) || defined(_MSC_VER))
 /* Windows */
-#if defined(__MINGW64__)
+# if defined(__MINGW64__)
 
-#define CONFIG_MODELICA_SPEC_PLATFORM "win64"
-#define CONFIG_OPENMODELICA_SPEC_PLATFORM "mingw64"
-#define CONFIG_GCC_DUMPMACHINE "x86_64-w64-mingw32"
-#define CONFIG_GCC_VERSION __VERSION__
+#  define CONFIG_MODELICA_SPEC_PLATFORM "win64"
+#  define CONFIG_OPENMODELICA_SPEC_PLATFORM "mingw64"
+#  define CONFIG_GCC_DUMPMACHINE "x86_64-w64-mingw32"
+#  define CONFIG_GCC_VERSION __VERSION__
 
-#elif defined(__MINGW32__)
+# elif defined(__MINGW32__)
 
-#define CONFIG_MODELICA_SPEC_PLATFORM "win32"
-#define CONFIG_OPENMODELICA_SPEC_PLATFORM "mingw32"
-#define CONFIG_GCC_DUMPMACHINE "i686-w64-mingw32"
-#define CONFIG_GCC_VERSION __VERSION__
+#  define CONFIG_MODELICA_SPEC_PLATFORM "win32"
+#  define CONFIG_OPENMODELICA_SPEC_PLATFORM "mingw32"
+#  define CONFIG_GCC_DUMPMACHINE "i686-w64-mingw32"
+#  define CONFIG_GCC_VERSION __VERSION__
 
-#elif defined(_MSV_VER) && defined(_M_IX86)
+# elif defined(_MSV_VER) && defined(_M_IX86)
 
-#define CONFIG_MODELICA_SPEC_PLATFORM "win32"
-#define CONFIG_OPENMODELICA_SPEC_PLATFORM "msvc32"
-#define CONFIG_GCC_DUMPMACHINE ""
-#define CONFIG_GCC_VERSION ""
+#  define CONFIG_MODELICA_SPEC_PLATFORM "win32"
+#  define CONFIG_OPENMODELICA_SPEC_PLATFORM "msvc32"
+#  define CONFIG_GCC_DUMPMACHINE ""
+#  define CONFIG_GCC_VERSION ""
 
-#elif defined(_MSV_VER) && defined(_M_X64)
+# elif defined(_MSV_VER) && defined(_M_X64)
 
-#define CONFIG_MODELICA_SPEC_PLATFORM "win64"
-#define CONFIG_OPENMODELICA_SPEC_PLATFORM "msvc64"
-#define CONFIG_GCC_DUMPMACHINE ""
-#define CONFIG_GCC_VERSION ""
+# define CONFIG_MODELICA_SPEC_PLATFORM "win64"
+# define CONFIG_OPENMODELICA_SPEC_PLATFORM "msvc64"
+# define CONFIG_GCC_DUMPMACHINE ""
+# define CONFIG_GCC_VERSION ""
 
-#endif
+# endif
 
 /* if we compiled omc with clang asume we
  * use it to compile simulation code with it
  */
-#if defined(__clang__)
-  #define DEFAULT_CC "clang"
-  #define DEFAULT_CXX "clang++"
-  #define DEFAULT_OMPCC "clang -fopenmp"
-  #define DEFAULT_LD "clang++"
-#else /* assume gcc */
-  #define DEFAULT_CC "gcc"
-  #define DEFAULT_CXX "g++"
-  #define DEFAULT_OMPCC "gcc -fopenmp"
-  #define DEFAULT_LD "g++"
-#endif
+# if defined(__clang__)
+#  define DEFAULT_CC "clang"
+#  define DEFAULT_CXX "clang++"
+#  define DEFAULT_OMPCC "clang -fopenmp"
+#  define DEFAULT_LD "clang++"
+# else /* assume gcc */
+#  define DEFAULT_CC "gcc"
+#  define DEFAULT_CXX "g++"
+#  define DEFAULT_OMPCC "gcc -fopenmp"
+#  define DEFAULT_LD "g++"
+# endif
 
 
-#define CONFIG_TRIPLE ""
-
-/* adrpo: add -loleaut32 as is used by ExternalMedia */
-#define DEFAULT_LDFLAGS "-fopenmp -Wl,-Bstatic -lregex -ltre -lintl -liconv -lexpat -lomcgc -lpthread -loleaut32 -limagehlp -lhdf5 -lz -lszip -Wl,-Bdynamic"
-
-#define CONFIG_WITH_OPENMP 1
-
-#define CONFIG_DEFAULT_OPENMODELICAHOME NULL
+# define CONFIG_TRIPLE ""
 
 /* adrpo: add -loleaut32 as is used by ExternalMedia */
-#define CONFIG_DLL_EXT ".dll"
-#define CONFIG_LPSOLVEINC "lpsolve/lp_lib.h"
+/* jdpipe: removing szip, seems not to be needed on current MSYS2 */
+/* FIXME these LDFLAGS should be formed using output from the local system's pkg-config outputs... */
+# define DEFAULT_LDFLAGS "-fopenmp -Wl,-Bstatic -lregex -ltre -lintl -liconv -lexpat -lomcgc -lpthread -loleaut32 -limagehlp -lhdf5 -lz -Wl,-Bdynamic"
 
-#if defined(__i386__) || defined(__x86_64__) || defined(_MSC_VER)
+# define CONFIG_WITH_OPENMP 1
+
+# define CONFIG_DEFAULT_OPENMODELICAHOME NULL
+
+/* adrpo: add -loleaut32 as is used by ExternalMedia */
+# define CONFIG_DLL_EXT ".dll"
+# define CONFIG_LPSOLVEINC "lpsolve/lp_lib.h"
+
+# if defined(__i386__) || defined(__x86_64__) || defined(_MSC_VER)
   /*
    * if we are on i386 or x86_64 or compiling with
    * Visual Studio then use the SSE instructions,
    * not the normal i387 FPU
    */
-  #define DEFAULT_CFLAGS "-Wno-parentheses-equality -falign-functions -mstackrealign -msse2 -mfpmath=sse ${MODELICAUSERCFLAGS}"
-#else
-  #define DEFAULT_CFLAGS "-Wno-parentheses-equality -falign-functions ${MODELICAUSERCFLAGS}"
-#endif
+#  define DEFAULT_CFLAGS "-Wno-parentheses-equality -falign-functions -mstackrealign -msse2 -mfpmath=sse ${MODELICAUSERCFLAGS}"
+# else
+#  define DEFAULT_CFLAGS "-Wno-parentheses-equality -falign-functions ${MODELICAUSERCFLAGS}"
+# endif
 
 /* for windows/mingw we don't need -fPIC for x86_64 target, also clang doesn't support it, gcc ignores it */
-#define DEFAULT_LINKER DEFAULT_LD" -shared -Xlinker --export-all-symbols"
+# define DEFAULT_LINKER DEFAULT_LD" -shared -Xlinker --export-all-symbols"
 
-#define CONFIG_IPOPT_INC /* Without IPOPT */
-#define CONFIG_IPOPT_LIB /* Without IPOPT */
+# define CONFIG_IPOPT_INC /* Without IPOPT */
+# define CONFIG_IPOPT_LIB /* Without IPOPT */
 
-#define WITH_HWLOC 0
-#define WITH_SUNDIALS
+# define WITH_HWLOC 0
+# define WITH_SUNDIALS
 
-#if defined(__MINGW32__)
-#define WITH_IPOPT
-#else
+# if defined(__MINGW32__)
+#  define WITH_IPOPT
+# else
 /* Without IPOPT for MSVC */
-#endif
+# endif
 
-#include "revision.h"
+# include "revision.h"
 
-#define WITH_SUITESPARSE
+# define WITH_SUITESPARSE
 
 /* On Windows (with OMDev) assume we have lapack*/
-#define HAVE_LAPACK
+# define HAVE_LAPACK
 /* On Windows (with OMDev) assume we have deprecated lapack functions*/
-#define HAVE_LAPACK_DEPRECATED
+# define HAVE_LAPACK_DEPRECATED
 
 #else /* Unix */ /* #if !defined(MSYS2_AUTOCONF) && (defined(__MINGW32__) || defined(_MSC_VER)) */
 
-#define CONFIG_GCC_DUMPMACHINE ""
-#define CONFIG_GCC_VERSION ""
-#define DEFAULT_LDFLAGS ""
+# define CONFIG_GCC_DUMPMACHINE ""
+# define CONFIG_GCC_VERSION ""
+# define DEFAULT_LDFLAGS ""
 
-#include "omc_config.unix.h"
+# include "omc_config.unix.h"
 
 #endif /* #if !defined(MSYS2_AUTOCONF) && (defined(__MINGW32__) || defined(_MSC_VER)) */
 
 #ifdef CONFIG_REVISION
-#define CONFIG_VERSION CONFIG_REVISION
+# define CONFIG_VERSION CONFIG_REVISION
 #else
-#define CONFIG_VERSION "unknown"
+# define CONFIG_VERSION "unknown"
 #endif
 
 
-#endif
+#endif /* OPENMODELICA_CONFIG_H */
